d4a63c67eadeb2f0d53286d2f80a9be64c2a69d3
Rune Botten <rbotten@gmail.com>
2020-09-22 13:46:46 +0200

    import project

---
+++ schemas/objects/openGraph.js
@@ -0,0 +1,27 @@
+export default {
+  type: 'object',
+  name: 'openGraph',
+  options: {
+    collapsible: true,
+    collapsed: true
+  },
+  fields: [
+    {
+      title: 'og:title',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      title: 'og:type',
+      name: 'type',
+      type: 'string',
+      description: "'website', or 'article' etc",
+    },
+    {
+      title: 'og:image',
+      name: 'image',
+      description: 'In your frontend you should make use of the asset URL',
+      type: 'image'
+    }
+  ]
+}
